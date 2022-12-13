7d2e9f9a836d819eb78511c18f8ebe71e94760ee
Tibo <thibault.giraudon@gmail.com>
2022-12-13 11:56:00 +0100

    change lib

--- libft/libft/ft_strncmp.c
+++ libft/libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: tgiraudo <tgiraudo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/06 20:13:58 by tgiraudo          #+#    #+#             */
-/*   Updated: 2022/11/13 11:05:03 by tgiraudo         ###   ########.fr       */
+/*   Updated: 2022/12/10 16:15:28 by tgiraudo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,13 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t	i;
-
-	i = 0;
-	if (n == 0)
-		return (0);
-	while (*s1 && *s2 && *s1 == *s2 && i < n - 1)
+	while ((*s1 || *s2) && (n > 0))
 	{
+		if (*s1 != *s2)
+			return ((unsigned char)*s1 - (unsigned char)*s2);
 		s1++;
 		s2++;
-		i++;
+		n--;
 	}
-	return ((unsigned char)*s1 - (unsigned char)*s2);
+	return (0);
 }
